fcea18c13497443ce3d0ca4f0b66311c8439ba25
Michael Johnston <lastobelus@mac.com>
2013-07-28 23:01:57 -0700

    add caching of time started checking credit

--- lib/shopify_unlimited.rb
+++ lib/shopify_unlimited.rb
@@ -1,3 +1,30 @@
 require "shopify_unlimited/version"
 require 'shopify_unlimited/active_resource/base_extensions'
 require 'shopify_unlimited/active_resource/connection_extensions'
+
+module ShopifyUnlimited
+  class << self
+    attr_accessor :use_memcached
+    def memcached
+      return nil unless use_memcached
+      @memcached ||= Dalli::Client.new()
+    end
+
+    def cache_key
+      URI.parse(ShopifyAPI::Base.site.to_s).host + "_api_reset_time"
+    end
+
+    def cached_time(max)
+      time = Time.now
+      time = memcached.cas(cache_key, 500) do |cached_time|
+        return time if cached_time.nil?
+        ((time - cached_time) > max) ? time : cached_time
+      end
+      time
+    end
+
+    def set_cached_time(time)
+      memcached.set(cache_key, time, 500)
+    end
+  end
+end
--- lib/shopify_unlimited/active_resource/connection_extensions.rb
+++ lib/shopify_unlimited/active_resource/connection_extensions.rb
@@ -27,11 +27,17 @@ module ::ActiveResource
     attr_reader :shopify_credit
     def handle_response_with_response_time_capture(response)
       handle_response_without_response_time_capture(response)
+      
       if(shopify_credit_header = response[SHOPIFY_CREDIT_LIMIT_HEADER_PARAM])
         used = shopify_credit_header.split('/').shift.to_i
 
-        if @shopify_credit.nil? || @shopify_credit.stale?(used)
+        if @shopify_credit.nil?
+          cached_time = ::ShopifyUnlimited.cached_time(SHOPIFY_CREDIT_LIMIT_PERIOD)
           @shopify_credit = ::ShopifyUnlimited::CreditUsed.new(used)
+          @shopify_credit.time = cached_time
+        elsif @shopify_credit.stale?(used)
+          @shopify_credit = ::ShopifyUnlimited::CreditUsed.new(used)
+          ::ShopifyUnlimited.set_cached_time(Time.now)
         else
           @shopify_credit.used = used
         end
